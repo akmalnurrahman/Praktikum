7f44368bbf4495e1ff50926344ff997b749b8073
akmalnurrahman <muhamad.akmal.tkom419@polban.ac.id>
2022-11-03 09:38:51 +0700

    praktikumscroll

---
+++ app/src/main/java/com/example/myapplication/model/Motivation.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+class Motivation (
+    @DrawableRes val imageResourceid: Int,
+    @StringRes val textResourceid: Int
+){
+}
